0cb103cfd828d56cb717c57713ffff68fa41ca60
Mikael <mikael.thiago@hotmail.com>
2020-07-26 12:45:59 -0300

    pasta para obtenção de dados da API

---
+++ src/services/api_config.js
@@ -0,0 +1,7 @@
+const API_KEY = "ca49e397662ae01678c188709bf85af1";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+
+const getAPIkey = () => API_KEY;
+const getImageBaseUrl = () => IMAGE_BASE_URL;
+
+export { getAPIkey, getImageBaseUrl };
