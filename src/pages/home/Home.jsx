b057701e71d54efdc6fd43ba6180772df16b0784
Mikael <mikael.thiago@hotmail.com>
2020-07-26 23:16:23 -0300

    página principal que faz o roteamento das views

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Principal from "./routes/Principal/Principal";
+
+import "./home.css"
+import Movie from "./routes/Movie/Movie";
+import Topbar from "./components/Topbar/Topbar";
+import SearchPage from "./routes/Search/Search";
+import Navbar from "./components/Navbar/Navbar";
+
+
+const Home = () => {
+
+    return (
+        <div className="home-root">
+            <Topbar />
+            <Navbar />
+            <div className="main-view">
+                <Switch>
+
+                    <Route path={"/movie/:id"}>
+                        <Movie />
+                    </Route>
+
+                    <Route path={"/search/:query/:page"}>
+                        <SearchPage />
+                    </Route>
+
+                    <Route exact path={""}>
+                        <Principal />
+                    </Route>
+
+                </Switch>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
