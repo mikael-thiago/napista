821caeddba88537aa2d512ab5f2cf9ba176ee216
Mikael <mikael.thiago@hotmail.com>
2020-07-27 18:01:03 -0300

    As chamadas agora são feitas para o servidor Node. Adicionadas mais chamadas

--- src/api-calls/api-calls.js
+++ src/api-calls/api-calls.js
@@ -1,44 +1,53 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.themoviedb.org/3/";
+const BASE_URL = "http://localhost:4000/";
 
-const getMostPopularMovies = async (api_key, page) => {
+const getMostPopularMovies = async (page) => {
 
-    const URL = BASE_URL + "movie/popular?api_key=" + api_key + "&page=" + page + "&language=pt";
+    const URL = BASE_URL + "movie/popular?page=" + page + "&language=pt";
 
     const result = (await axios.get(URL));
 
     return result.data.results;
 }
 
-const getConfiguration = async (api_key) => {
-    const URL = BASE_URL + "configuration?api_key=" + api_key;
+const getMovie = async (movie_id) => {
 
-    const result = (await axios.get(URL));
-    return result.data;
-}
-
-const getMovie = async (api_key, movie_id) => {
-    const MOVIE_URL = BASE_URL + "movie/" + movie_id + "?api_key=" + api_key + "&language=pt";
+    const MOVIE_URL = BASE_URL + "movie/" + movie_id + "?language=pt";
 
     let movieResult = (await axios.get(MOVIE_URL));
-
-    const CREDITS_URL = BASE_URL + "movie/" + movie_id + "/credits?api_key=" + api_key + "&language=pt"
-
-    let creditsResult = (await axios.get(CREDITS_URL));
-
-    movieResult.data.cast = creditsResult.data.cast;
 
     return movieResult.data;
 }
 
-const getSearchResult = async (api_key, query, page) => {
-    const SEARCH_URL = BASE_URL + "search/movie?api_key=" + api_key + "&language=pt&query=" + query + "&page=" + page;
+const getFavoritedMovies = async () => {
+    const FAVORITED_MOVIES_URL = BASE_URL + "movie/favoriteds?language=pt";
+
+    const result = (await axios.get(FAVORITED_MOVIES_URL));
+
+    return result.data;
+}
+
+const favoriteMovie = async (movie_id) => {
+    const FAVORITE_URL = BASE_URL + "movie/favorite/" + movie_id;
+
+    const result = (await axios.post(FAVORITE_URL));
+
+}
+
+const unfavoriteMovie = async (movie_id) => {
+    const UNFAVORITE_URL = BASE_URL + "movie/unfavorite/" + movie_id;
+
+    const result = (await axios.delete(UNFAVORITE_URL));
+}
+
+const getSearchResult = async (query, page) => {
+
+    const SEARCH_URL = BASE_URL + "search/movie?query=" + query + "&page=" + page + "&language=pt";
 
     let searchResult = (await axios.get(SEARCH_URL));
 
     return searchResult.data;
 }
 
-
-export { getMostPopularMovies, getConfiguration, getMovie, getSearchResult };
+export { getMostPopularMovies, getMovie, favoriteMovie, unfavoriteMovie, getSearchResult, getFavoritedMovies };
