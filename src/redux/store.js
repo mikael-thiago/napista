35bfcbd6efdc38962078a350a3497bf92c018e3b
Mikael <mikael.thiago@hotmail.com>
2020-07-27 17:54:06 -0300

    removidos diretórios do redux, não será necessário

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from "./slices/searchSlice.js";
 
 export default configureStore({
     reducer: {
+        search: searchReducer
     }
 })
