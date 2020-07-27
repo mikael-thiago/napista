0ccf68f5e22c0942d38c52a0c69250aa5ba13c06
Mikael <mikael.thiago@hotmail.com>
2020-07-26 23:17:23 -0300

    componentes utilizados na página Home

---
+++ src/pages/home/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import { NavbarButton } from "../Topbar/Topbar";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-header">
+                <NavbarButton />
+            </div>
+            <div className="navbar-body">
+                <nav className="navbar-nav">
+                    <ul>
+                        <li className="navbar-item">
+                            <Link className="navbar-link" to="">
+                                <span className="navbar-item-icon glyphicon glyphicon-home"></span>
+                                <div className="navbar-item-text">Início</div>
+                            </Link>
+                        </li>
+
+                        <li className="navbar-item">
+                            <Link className="navbar-link" to="/favoritos">
+                                <span className="navbar-item-icon glyphicon glyphicon-heart"></span>
+                                <div className="navbar-item-text">Favoritos</div>
+                            </Link>
+                        </li>
+
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/home/components/Topbar/Topbar.jsx
@@ -0,0 +1,63 @@
+import React, { useRef } from "react";
+import "./topbar.css";
+
+import { withRouter } from "react-router-dom";
+
+const SearchInput = withRouter(({ history, match }) => {
+
+    const inputRef = useRef();
+
+    const query = match.params.query;
+
+    const handleInputChange = (e) => {
+
+        if (e.target.value !== "") {
+            history.push("/search/" + e.target.value + "/1");
+        } else {
+            history.push("/");
+        }
+
+    }
+
+    return (
+        <div className="search-input-wrapper">
+            <span className="search-input-icon glyphicon glyphicon-search"></span>
+
+            <input type="text" ref={inputRef} value={query} onChange={handleInputChange} placeholder="Busque aqui seus filmes" className="search-input">
+            </input>
+        </div>
+
+    );
+});
+
+const NavbarButton = () => {
+
+    const toggleNavbar = () => {
+        let navbar = document.getElementsByClassName("navbar")[0];
+
+        console.log(navbar.style);
+        if (!navbar.style.transform)
+            navbar.style.transform = "translateX(0)";
+        else
+            navbar.style.transform = "";
+
+    }
+
+    return (
+        <button onClick={toggleNavbar} className="navbar-button">
+            <span className="glyphicon glyphicon-menu-hamburger"></span>
+        </button>
+    )
+}
+
+const Topbar = () => {
+    return (
+        <div className="topbar">
+            <NavbarButton />
+            <SearchInput />
+        </div>
+    )
+}
+
+export { NavbarButton };
+export default Topbar;
