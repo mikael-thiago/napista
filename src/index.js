9317e081970f72bc7e9db120f44f8dd01ea74431
Mikael <mikael.thiago@hotmail.com>
2020-07-27 18:02:49 -0300

    removida store do redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from "./pages/home/Home";
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Home />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
