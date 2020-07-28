83c73cc4b01a5ff717a217f2e32f273d6c2ea89c
Mikael <mikael.thiago@hotmail.com>
2020-07-27 22:18:14 -0300

    carousel não exibe mais controllers caso não seja necessário

--- src/pages/home/UIComponents/Carousel/Carousel.jsx
+++ src/pages/home/UIComponents/Carousel/Carousel.jsx
@@ -1,19 +1,27 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 //Styles
 import "./carousel.css";
 
-const Carousel = ({ cast, itemWidth = 100, children }) => {
-    const contentRef = useRef();
+const Carousel = ({ itemWidth = 100, title = "", children }) => {
+
+    const contentRef = useRef(null);
+    const [isOverflowed, setIsOverflowed] = useState(false);
+
+    useEffect(() => {
+
+        setIsOverflowed(contentRef.current.scrollWidth > contentRef.current.clientWidth);
+
+    }, [contentRef.current])
 
     return (
         <section className="carousel">
             <div className="carousel-span">
                 <div className="carousel-title">
-                    Elenco
+                    {title}
                 </div>
             </div>
-            <button className="carousel-controller" onClick={() => { contentRef.current.scrollLeft -= itemWidth * 5 }} style={{ position: "absolute", left: "0", zIndex: 2 }}>
+            <button className="carousel-controller" onClick={() => { contentRef.current.scrollLeft -= itemWidth * 5 }} style={{ position: "absolute", left: "0", zIndex: 2, display: (isOverflowed ? "" : "none") }}>
                 <span className="glyphicon glyphicon-chevron-left"></span>
             </button>
 
@@ -21,7 +29,7 @@ const Carousel = ({ cast, itemWidth = 100, children }) => {
                 {children}
             </div>
 
-            <button className="carousel-controller" onClick={() => { contentRef.current.scrollLeft += itemWidth * 5 }} style={{ position: "absolute", right: "0", zIndex: 2 }}>
+            <button className="carousel-controller" onClick={() => { contentRef.current.scrollLeft += itemWidth * 5 }} style={{ position: "absolute", right: "0", zIndex: 2, display: (isOverflowed ? "" : "none") }}>
                 <span className="glyphicon glyphicon-chevron-right"></span>
             </button>
         </section>
