ccd7d50b594b292d7ce59efd8f0976683be6a8bb
Mikael <mikael.thiago@hotmail.com>
2020-07-28 06:02:55 -0300

    retirado o servidor node, gerenciamento de favoritos agora é feito em local storage

--- node/src/api_config/api_config.js
+++
@@ -1,7 +0,0 @@
-const API_CONFIG = {
-    "API_KEY": "ca49e397662ae01678c188709bf85af1",
-    "IMAGE_BASE_URL": "https://image.tmdb.org/t/p/",
-    "API_BASE_URL": "https://api.themoviedb.org/3/"
-}
-
-module.exports = API_CONFIG;
--- node/src/routes/search.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const search = require("../controllers/search.controller.js");
-
-router.get("/movie", search.getSearchResult);
-
-module.exports = router;
