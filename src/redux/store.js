5ac6f0fc8c4c42229859eaac64873f2972c2f264
Mikael <mikael.thiago@hotmail.com>
2020-07-28 00:28:03 -0300

    removidas pastas do redux, foi usado apenas hooks

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import searchReducer from "./slices/searchSlice.js";
-
-export default configureStore({
-    reducer: {
-        search: searchReducer
-    }
-})
